4091654503d52fb219f0c5477dfed21c8e7ff5d0
Aureate Team <hello@aureatelabs.com>
2022-01-12 14:32:40 +0530

    Initialize Practical/EstimateProfit extension

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aureate Labs Pvt Ltd.
+ *
+ * Do not edit or add to this file if you wish to upgrade to newer versions in the future.
+ * If you wish to customize this module for your needs.
+ * Please contact us https://aureatelabs.com/contact-us.
+ *
+ * @category   Practical
+ * @package    Practical_EstimateProfit
+ * @author     Yogesh Suhagiya (yksuhagiya@gmail.com)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Practical_EstimateProfit',
+    __DIR__
+);
